78361bf188eb776afc1785382e517d52b815b14b
Minecraft Gamer Nation <minecraft-gamer-nation@post.com>
2026-06-17 21:38:00 -0700

    Fix 403 error on static host by adding trailingSlash

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: process.env.BUILD_STATIC === 'true' ? 'export' : undefined,
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
