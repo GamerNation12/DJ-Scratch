0ef95e81b9507cd6e0e49462c9a3a7c69d048b6d
Minecraft Gamer Nation <minecraft-gamer-nation@post.com>
2026-06-24 12:59:52 -0700

    chore: remove deprecated scratch and migration scripts

--- scripts/test.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config({path: 'web/.env.local'}); 
-const postgres = require('postgres'); 
-const sql = postgres(process.env.DATABASE_URL); 
-sql`SELECT user_id, discord_username FROM user_settings`.then(console.log).then(() => process.exit(0));
