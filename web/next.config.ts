c918aadfa273b690d4266770b5037f608b1f169f
Minecraft Gamer Nation <minecraft-gamer-nation@post.com>
2026-06-23 22:32:09 -0700

    Fix APK download headers

--- web/next.config.ts
+++ web/next.config.ts
@@ -4,6 +4,23 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*).apk",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/vnd.android.package-archive",
+          },
+          {
+            key: "Content-Disposition",
+            value: "attachment",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
