0d68f1bcb36c03106d8c600751d2b3b48d5266b2
Minecraft Gamer Nation <minecraft-gamer-nation@post.com>
2026-07-02 21:03:42 -0700

    fix: load social cog in events.py for /reply command

--- discord-bot/src/core/events.py
+++ discord-bot/src/core/events.py
@@ -354,7 +354,7 @@ async def setup_hook():
             bot.add_custom_reactions = add_custom_reactions
             bot.save_user = save_user
 
-            cogs = ['cogs.admin', 'src.commands.admin_ipc', 'src.commands.lastfm', 'src.commands.importer', 'src.commands.settings', 'src.commands.info', 'src.commands.spotify_remote']
+            cogs = ['cogs.admin', 'src.commands.admin_ipc', 'src.commands.lastfm', 'src.commands.importer', 'src.commands.settings', 'src.commands.info', 'src.commands.spotify_remote', 'src.commands.social']
             for cog in cogs:
                 try:
                     await bot.load_extension(cog)
